7484e336018e6372b1f410160b887e695cdd3d02
赵尉尉 <1165933479@qq.com>
2018-05-07 22:30:42 +0800

    adapter

--- arch-mvvm/src/main/java/io/auxo/arch/mvvm/view/adapter/BaseListAdapter.java
+++ arch-mvvm/src/main/java/io/auxo/arch/mvvm/view/adapter/BaseListAdapter.java
@@ -1,26 +1,89 @@
 package io.auxo.arch.mvvm.view.adapter;
 
+import android.support.annotation.NonNull;
 import android.widget.BaseAdapter;
 
+import java.util.Collection;
 import java.util.List;
 
 public abstract class BaseListAdapter<T> extends BaseAdapter {
 
-    private List<T> mData;
+    private List<T> items;
+
+    public BaseListAdapter(List<T> items) {
+        this.items = items;
+    }
 
     @Override
     public int getCount() {
-        return mData == null ? 0 : mData.size();
+        return items.size();
     }
 
     @Override
     public T getItem(int position) {
-        return mData.get(position);
+        return items.get(position);
     }
 
     @Override
     public long getItemId(int position) {
         return position;
+    }
+
+    public boolean add(T t) {
+        return notifyDataSetChanged(items.add(t));
+    }
+
+    public boolean remove(Object o) {
+        return notifyDataSetChanged(items.remove(o));
+    }
+
+    public boolean containsAll(@NonNull Collection<?> c) {
+        return notifyDataSetChanged(items.containsAll(c));
+    }
+
+    public boolean addAll(@NonNull Collection<? extends T> c) {
+        return notifyDataSetChanged(items.addAll(c));
+    }
+
+    public boolean addAll(int index, @NonNull Collection<? extends T> c) {
+        return notifyDataSetChanged(items.addAll(index, c));
+    }
+
+    public boolean removeAll(@NonNull Collection<?> c) {
+        return notifyDataSetChanged(items.removeAll(c));
+    }
+
+    public boolean retainAll(@NonNull Collection<?> c) {
+        return notifyDataSetChanged(items.retainAll(c));
+    }
+
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
+
+    public T set(int index, T element) {
+        T preElement = items.set(index, element);
+        notifyDataSetChanged();
+        return preElement;
+    }
+
+    public void add(int index, T element) {
+        items.add(index, element);
+        notifyDataSetChanged();
+    }
+
+    public T remove(int index) {
+        T element = items.remove(index);
+        notifyDataSetChanged();
+        return element;
+    }
+
+    protected boolean notifyDataSetChanged(boolean should) {
+        if (should) {
+            notifyDataSetChanged();
+        }
+        return should;
     }
 
 }
---
+++ arch-mvvm/src/main/java/io/auxo/arch/mvvm/view/adapter/MvvmRecyclerAdapter.java
@@ -0,0 +1,29 @@
+package io.auxo.arch.mvvm.view.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class MvvmRecyclerAdapter<T> extends RecyclerView.Adapter {
+
+    private List<T> items;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
